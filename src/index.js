531264e8a565a85c60a7c005b3c4f44265781952
Axel Isouard <axel@isouard.fr>
2018-03-18 19:07:50 +0100

    Main: Remove commander for now

--- src/index.js
+++ src/index.js
@@ -2,20 +2,11 @@
 
 const Server = require('./server');
 const WsServerSocket = require('./sockets/wsServerSocket');
-const program = require('commander');
-
-program
-  .version('1.0.0')
-  .option('-b, --bind', 'Bind URL')
-  .option('-d, --database-url', 'Database URL')
-  .option('-s, --store-url', 'Store URL')
-  .option('-l, --log-file', 'Path to log file')
-  .parse(process.argv);
 
 const server = new Server();
 const socket = new WsServerSocket(server, { port: 7777 });
-socket.listen();
 
+socket.listen();
 process.on('SIGINT', () => {
   socket.close();
 });
